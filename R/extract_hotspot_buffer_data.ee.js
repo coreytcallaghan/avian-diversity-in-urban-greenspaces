8fdcf31dabe4dac5d653c66bec586b906b3e514b
Mitchell Lyons <mitchell.lyons@gmail.com>
2017-09-07 15:49:27 +1000

    update hs table / new hansen layers folder

--- R/extract_hotspot_buffer_data.ee.js
+++ R/extract_hotspot_buffer_data.ee.js
@@ -1,7 +1,7 @@
-var buf_5km = ee.FeatureCollection('ft:18iqj8u-8r1Al3wIwYkjYrvRUXMhK7akhI4chptmS');
-var buf_15km = ee.FeatureCollection('ft:1hDo8aPeIP71Ctv25jgDOzffhkjN_LQ_B1yFlkHfC');
-var buf_25km = ee.FeatureCollection('ft:1uLO_0i0MddINBTaxwGWY6NvhQ8J8Q1pcvjxNVKTk');
-var hotspots = ee.FeatureCollection('ft:16orJ7hYj5JBn2V4gPG8Q3YaA1uxREapv-dAB63ro');
+//var buf_5km = ee.FeatureCollection('ft:18iqj8u-8r1Al3wIwYkjYrvRUXMhK7akhI4chptmS');
+//var buf_15km = ee.FeatureCollection('ft:1hDo8aPeIP71Ctv25jgDOzffhkjN_LQ_B1yFlkHfC');
+//var buf_25km = ee.FeatureCollection('ft:1uLO_0i0MddINBTaxwGWY6NvhQ8J8Q1pcvjxNVKTk');
+var hotspots = ee.FeatureCollection('users/mitchest/ebird_hotspots/eb_final_hotspots');
 
 //Map.addLayer(buf_5km);
 //Map.addLayer(buf_15km);
@@ -9,8 +9,8 @@ var hotspots = ee.FeatureCollection('ft:16orJ7hYj5JBn2V4gPG8Q3YaA1uxREapv-dAB63r
 //Map.addLayer(hotspots);
 
 // load forest cover
-var forest_loss = ee.Image('UMD/hansen/global_forest_change_2015').select('loss');
-var forest_gain = ee.Image('UMD/hansen/global_forest_change_2015').select('gain');
+var forest_loss = ee.Image('UMD/hansen/global_forest_change_2015_v1_3').select('loss');
+var forest_gain = ee.Image('UMD/hansen/global_forest_change_2015_v1_3').select('gain');
 //Map.addLayer({eeObject:forest_loss, name:"hansen-forest-loss"});
 
 // load GLS (USGS/NASA)
@@ -121,54 +121,54 @@ var export_buffer_stats = function(buf_stats, out_name) {
 //export_buffer_stats(reduce_by_buffer(forest_loss, buf_5km), 'forest_loss_5');
 //export_buffer_stats(reduce_by_buffer(forest_loss, buf_15km), 'forest_loss_15');
 //export_buffer_stats(reduce_by_buffer(forest_loss, buf_25km), 'forest_loss_25');
-//export_buffer_stats(reduce_by_buffer(forest_loss, hotspots), 'forest_loss_hs');
+export_buffer_stats(reduce_by_buffer(forest_loss, hotspots), 'forest_loss_hs');
 
 //export_buffer_stats(reduce_by_buffer(forest_gain, buf_5km), 'forest_gain_5');
 //export_buffer_stats(reduce_by_buffer(forest_gain, buf_15km), 'forest_gain_15');
 //export_buffer_stats(reduce_by_buffer(forest_gain, buf_25km), 'forest_gain_25');
-//export_buffer_stats(reduce_by_buffer(forest_gain, hotspots), 'forest_gain_hs');
+export_buffer_stats(reduce_by_buffer(forest_gain, hotspots), 'forest_gain_hs');
 
 // gls_tree
 //export_buffer_stats(reduce_by_buffer(gls_tree, buf_5km), 'gls_tree_5');
 //export_buffer_stats(reduce_by_buffer(gls_tree, buf_15km), 'gls_tree_15');
 //export_buffer_stats(reduce_by_buffer(gls_tree, buf_25km), 'gls_tree_25');
-//export_buffer_stats(reduce_by_buffer(gls_tree, hotspots), 'gls_tree_hs');
+export_buffer_stats(reduce_by_buffer(gls_tree, hotspots), 'gls_tree_hs');
 
 // gls_water
 //export_buffer_stats(reduce_by_buffer_value(gls_water, buf_5km, 2), 'gls_water_5');
 //export_buffer_stats(reduce_by_buffer_value(gls_water, buf_15km, 2), 'gls_water_15');
 //export_buffer_stats(reduce_by_buffer_value(gls_water, buf_25km, 2), 'gls_water_25');
-//export_buffer_stats(reduce_by_buffer_value(gls_water, hotspots, 2), 'gls_water_hs');
+export_buffer_stats(reduce_by_buffer_value(gls_water, hotspots, 2), 'gls_water_hs');
 
 // evi
 //export_buffer_stats(reduce_by_buffer(evi_2013, buf_5km), 'evi_2013_5');
 //export_buffer_stats(reduce_by_buffer(evi_2013, buf_15km), 'evi_2013_15');
 //export_buffer_stats(reduce_by_buffer(evi_2013, buf_25km), 'evi_2013_25');
-//export_buffer_stats(reduce_by_buffer(evi_2013, hotspots), 'evi_2013_hs');
+export_buffer_stats(reduce_by_buffer(evi_2013, hotspots), 'evi_2013_hs');
 
 //export_buffer_stats(reduce_by_buffer(evi_2014, buf_5km), 'evi_2014_5');
 //export_buffer_stats(reduce_by_buffer(evi_2014, buf_15km), 'evi_2014_15');
 //export_buffer_stats(reduce_by_buffer(evi_2014, buf_25km), 'evi_2014_25');
-//export_buffer_stats(reduce_by_buffer(evi_2014, hotspots), 'evi_2014_hs');
+export_buffer_stats(reduce_by_buffer(evi_2014, hotspots), 'evi_2014_hs');
 
 //export_buffer_stats(reduce_by_buffer(evi_2015, buf_5km), 'evi_2015_5');
 //export_buffer_stats(reduce_by_buffer(evi_2015, buf_15km), 'evi_2015_15');
 //export_buffer_stats(reduce_by_buffer(evi_2015, buf_25km), 'evi_2015_25');
-//export_buffer_stats(reduce_by_buffer(evi_2015, hotspots), 'evi_2015_hs');
+export_buffer_stats(reduce_by_buffer(evi_2015, hotspots), 'evi_2015_hs');
 
 //export_buffer_stats(reduce_by_buffer(evi_2016, buf_5km), 'evi_2016_5');
 //export_buffer_stats(reduce_by_buffer(evi_2016, buf_15km), 'evi_2016_15');
 //export_buffer_stats(reduce_by_buffer(evi_2016, buf_25km), 'evi_2016_25');
-//export_buffer_stats(reduce_by_buffer(evi_2016, hotspots), 'evi_2016_hs');
+export_buffer_stats(reduce_by_buffer(evi_2016, hotspots), 'evi_2016_hs');
 
 //export_buffer_stats(reduce_by_buffer(evi_2017, buf_5km), 'evi_2017_5');
 //export_buffer_stats(reduce_by_buffer(evi_2017, buf_15km), 'evi_2017_15');
 //export_buffer_stats(reduce_by_buffer(evi_2017, buf_25km), 'evi_2017_25');
-//export_buffer_stats(reduce_by_buffer(evi_2017, hotspots), 'evi_2017_hs');
+export_buffer_stats(reduce_by_buffer(evi_2017, hotspots), 'evi_2017_hs');
 
 // esa glob cover
 //export_buffer_stats(reduce_by_buffer_mode(esa_globcover, buf_5km), 'esa_globcover_5');
 //export_buffer_stats(reduce_by_buffer_mode(esa_globcover, buf_15km), 'esa_globcover_15');
 //export_buffer_stats(reduce_by_buffer_mode(esa_globcover, buf_25km), 'esa_globcover_25');
-//export_buffer_stats(reduce_by_buffer_mode(esa_globcover, hotspots), 'esa_globcover_hs');
+export_buffer_stats(reduce_by_buffer_mode(esa_globcover, hotspots), 'esa_globcover_hs');
 
