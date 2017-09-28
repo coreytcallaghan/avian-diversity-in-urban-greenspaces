39fcd34319daff9a784f58b8f4253de799d22dd5
Corey Callaghan <callaghan.corey.t@gmail.com>
2017-09-28 10:59:36 +1000

    updated R file for creating modelling data

--- R/extracting response and predictor variables for modelling.R
+++ R/extracting response and predictor variables for modelling.R
@@ -32,8 +32,7 @@ habitat_variables <- bind_rows(myfiles)
 rm(temp)
 rm(myfiles)
 
-## setwd('H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Github work/avian diversity in greenspaces')
-
+## setwd("H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Analysis/avian diversity in greenspaces")
 
 
 #### for hotspots
@@ -49,20 +48,17 @@ rm(temp)
 rm(myfiles)
 
 
-## setwd('H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Github work/avian diversity in greenspaces')
+## setwd("H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Analysis/avian diversity in greenspaces")
 
 ## Now, lets' combine data to have __response variables__ & __predictor variables__
 
 ### Forming predictor variables file for the buffers
 predictor_variables.buffers <- habitat_variables %>%
-  # get rid of 'name'
-  select(-one_of('name')) %>%
+  rename(LOCALITY_ID = LOCALITY_I) %>%
   # transpose file
   dcast(LOCALITY_ID ~ file.name, value.var="value") %>%
-  # remove 'NA' column
-  select(-one_of('NA')) %>%
   # join with possible study sites file from above
-  inner_join(., possible_study_sites, by="LOCALITY_ID")
+  inner_join(., Final_study_sites, by="LOCALITY_ID")
 
 ### Taking a mean of the included evi year values
 predictor_variables.buffers$evi_5 <- rowMeans(predictor_variables.buffers[, c('evi_2013_5', 'evi_2014_5', 'evi_2015_5', 'evi_2016_5')])
@@ -75,6 +71,7 @@ predictor_variables.buffers$evi_25 <- rowMeans(predictor_variables.buffers[, c('
 
 ### Forming predictor variables file for the hotspots
 predictor_variables.hotspots <- hotspot_variables %>%
+  rename(LOCALITY_ID = LOCALITY_I) %>%
   # transpose file
   dcast(LOCALITY_ID ~ file.name, value.var="value")
 
@@ -84,6 +81,9 @@ predictor_variables.hotspots$evi_hs <- rowMeans(predictor_variables.hotspots[, c
 ### making one single predictor variables file
 predictor_variables <- merge(predictor_variables.buffers, predictor_variables.hotspots, by="LOCALITY_ID")
 
+predictor_variables <- predictor_variables %>%
+  rename(Polygon_id = LOCALITY_ID)
+
 
 rm(predictor_variables.buffers)
 rm(predictor_variables.hotspots)
@@ -92,9 +92,7 @@ rm(hotspot_variables)
 
 
 ### Forming response variables file
-
-## I want to have two different response variables; __species richness__ and __species diversity__
-
+## I want to have two different response variables:  __species richness__ and __species diversity__
 #### Let's create a file with the response variables for each checklist
 
 # Counts how many 'x's per checklist
@@ -106,7 +104,7 @@ X_missing <- bird_data %>%
 sampling_event_info <- bird_data %>%
   select(SAMPLING_EVENT_IDENTIFIER, LOCALITY, LOCALITY_ID, OBSERVATION_DATE,
          PROTOCOL_TYPE, ALL_SPECIES_REPORTED, EFFORT_DISTANCE_KM, EFFORT_AREA_HA, 
-         DURATION_MINUTES, OBSERVATION_DATE) %>%
+         DURATION_MINUTES, OBSERVATION_DATE, GROUP_IDENTIFIER) %>%
   distinct()
 
 # Formatting Date
@@ -122,81 +120,75 @@ bird_data.new <- bird_data %>%
   inner_join(., X_missing, by="SAMPLING_EVENT_IDENTIFIER")
 
 
-## First, calculate species richness and abundance
+# Apply some filtering criteria
+bird_data.new <- bird_data.new %>%
+  filter(ALL_SPECIES_REPORTED==1) %>%
+  filter(PROTOCOL_TYPE %in% c("eBird - Exhaustive Area Count", "eBird - Stationary Count", "eBird - Traveling Count")) %>%
+  filter(OBSERVATION_DATE>= "2013-01-01" & OBSERVATION_DATE <= "2016-12-31")
+
+# filter out group_identifier data to eliminate duplicated checklists
+duplicated <- bird_data.new %>%
+  drop_na(GROUP_IDENTIFIER) %>%
+  select(GROUP_IDENTIFIER, SAMPLING_EVENT_IDENTIFIER) %>%
+  distinct(.keep_all=TRUE) %>%
+  group_by(GROUP_IDENTIFIER) %>%
+  # randomly sample one checklist for each group_identifier
+  sample_n(., 1) %>%
+  .$SAMPLING_EVENT_IDENTIFIER
+
+duplicated_data <- bird_data.new %>%
+  filter(SAMPLING_EVENT_IDENTIFIER %in% duplicated)
+
+## now, append the selected checklists for each group_identifier
+## with the non group_identifier checklists from the data
+
+# first select all data without a group_identifier
+bird_data.new <- bird_data.new %>%
+  filter(!grepl("G", GROUP_IDENTIFIER)) %>%
+  bind_rows(., duplicated_data)
+
+##############################################################################
+############ apply distance and duration caps ################################
+##############################################################################
+bird_data.new <- bird_data.new %>%
+  filter(DURATION_MINUTES >= 5 & DURATION_MINUTES <=240) %>%
+  filter(EFFORT_DISTANCE_KM <= 10)
+
+## Calculate species richness and diversity
+
+## First, calculate species richness
 species_richness <- bird_data.new %>%
   group_by(SAMPLING_EVENT_IDENTIFIER) %>%
-  summarise(species_richness=length(unique(COMMON_NAME)))
+  summarise(checklist_species_richness=length(unique(COMMON_NAME))) %>%
+  inner_join(., sampling_event_info, by="SAMPLING_EVENT_IDENTIFIER") %>%
+  select(-one_of("LOCALITY"))
 
 ## Second, calculate species diversity
 species_diversity <- bird_data.new %>%
-  filter(!is.na(OBSERVATION_COUNT)) %>%
+  filter(number_X == 0) %>%
   group_by(SAMPLING_EVENT_IDENTIFIER) %>%
-  summarise(species_diversity=diversity(OBSERVATION_COUNT))
+  summarise(species_diversity=diversity(OBSERVATION_COUNT)) %>%
+  inner_join(., sampling_event_info, by="SAMPLING_EVENT_IDENTIFIER") %>%
+  select(-one_of("LOCALITY"))
 
-# Merging the species richness and species diversity dataframes with sampling event info
-response_variables <- sampling_event_info %>%
-  inner_join(., species_diversity, by="SAMPLING_EVENT_IDENTIFIER") %>%
-  inner_join(., species_richness, by="SAMPLING_EVENT_IDENTIFIER") %>%
-  ## filtering data to highest quality
-  filter(ALL_SPECIES_REPORTED == 1, 
-         PROTOCOL_TYPE !="eBird - Casual Observation",
-         PROTOCOL_TYPE !="Historical") %>%
-  filter(OBSERVATION_DATE>= "2011-01-01" & OBSERVATION_DATE <= "2016-12-31") 
 
-## adding month to the response variables file
+## adding month to the response variables files
 detach("package:data.table", unload=TRUE)
-response_variables$month <- month(response_variables$OBSERVATION_DATE)
+species_diversity$month <- month(species_diversity$OBSERVATION_DATE)
+species_richness$month <- month(species_richness$OBSERVATION_DATE)
 
-rm(species_diversity)
-rm(species_richness)
-rm(bird_data)
-rm(bird_data.new)
-rm(sampling_event_info)
-rm(X_missing)
+species_richness <- species_richness %>%
+  inner_join(., Final_study_sites, by="LOCALITY_ID")
 
-rm(possible_study_sites)
-
-## Make a single modelling.data file
-modelling.data <- merge(predictor_variables, response_variables, by="LOCALITY_ID")
-
-rm(predictor_variables, response_variables)
-
-## histogram of main response variable
-hist(modelling.data$species_richness)
+species_diversity <- species_diversity %>%
+  inner_join(., Final_study_sites, by="LOCALITY_ID")
 
 
-## try a log transform
-hist(log(modelling.data$species_richness))
-
-
-## Looks good! Looks to be mostly normal, after a log transformation.
-
-## Number of data points by location
-modelling.data %>%
-  group_by(LOCALITY_ID) %>%
-  summarise(N=length(unique(SAMPLING_EVENT_IDENTIFIER))) %>%
-  arrange(N) %>%
-  ggplot(., aes(x=LOCALITY_ID, y=N))+
-  geom_bar(stat='identity')+
-  coord_flip()
-
-## Obviously a problem given there is substantially more data from one site compared to the others!
-## Need to account for this in the modelling, but first let's remove the smallest of sample sizes
-
-### Now we have the data to start modelling, but first 
-### Let's remove any locations with <20 checklists - based on previous work
-
-filtered_locations <- modelling.data %>%
-  group_by(LOCALITY_ID) %>%
-  summarise(N=length(unique(SAMPLING_EVENT_IDENTIFIER))) %>%
-  arrange(N) %>%
-  filter(N>=20) %>%
-  .$LOCALITY_ID
-
-modelling.data <- modelling.data %>%
-  filter(LOCALITY_ID %in% filtered_locations)
-
-rm(filtered_locations)
-
+### remove all but the two response variables and the predictor variables file, and final study sites
+rm(list=setdiff(ls(), c("species_richness", "species_diversity", "predictor_variables", "Final_study_sites")))
 
 save.image("Data/modelling_data.RData")
+
+
+
+
