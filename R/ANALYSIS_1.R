ed84a0ff32761d28e84d81a16db9423802f0b210
Corey Callaghan <callaghan.corey.t@gmail.com>
2017-10-06 08:15:27 +1100

    analyses scripts

---
+++ R/ANALYSIS_1.R
@@ -0,0 +1,257 @@
+#########################################################
+#########################################################
+################  ANALYSIS 1 ############################
+#########################################################
+#########################################################
+
+
+## setwd("H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Analysis/avian diversity in greenspaces")
+
+
+## First, load the RData file in
+load("Data/modelling_data.RData")
+
+### Read in bird data
+load("Data/Bird_data/bird_data.RData")
+
+### Packages
+library(dplyr)
+library(ggplot2)
+library(lme4)
+library(arm)
+library(MuMIn)
+library(effects)
+library(mgcv)
+
+### Prepare data for modelling of "FINAL DATASET"
+
+## collapse all sites to polygon ids
+analysis_all <- predictor_variables
+analysis_all$species_richness <- as.numeric(analysis_all$species_richness)
+analysis_all <- na.exclude(analysis_all)
+
+### Preliminary EDA for RESULTS section of paper
+## Number of sites
+length(unique(analysis_all$Polygon_id))
+
+## Number of cities
+length(unique(analysis_all$Urban_area))
+
+## comparing richness and checklists among dataset 1
+location_richness <- Final_study_sites %>%
+  dplyr::select(Polygon_id, LOCALITY_ID) %>%
+  right_join(., analysis_all, by="Polygon_id") %>%
+  inner_join(., bird_data, by="LOCALITY_ID") %>%
+  group_by(Polygon_id) %>%
+  filter(CATEGORY %in% c("species", "issf")) %>%
+  summarise(species_richness = length(unique(COMMON_NAME)),
+            checklists=length(unique(SAMPLING_EVENT_IDENTIFIER)))
+
+mean(location_richness$species_richness)
+sd(location_richness$species_richness)
+min(location_richness$species_richness)
+max(location_richness$species_richness)
+
+mean(location_richness$checklists)
+sd(location_richness$checklists)
+min(location_richness$checklists)
+max(location_richness$checklists)
+
+## test for correlation between richness and checklists
+hist(location_richness$species_richness)
+hist(location_richness$checklists)
+hist(sqrt(location_richness$checklists))
+
+cor.test(location_richness$species_richness, sqrt(location_richness$checklists))
+
+
+#####################################################
+############# MODELLING #############################
+#####################################################
+
+### test for correlation among predictor variables
+global_predictors <- analysis_all %>%
+  dplyr::select(Area_ha, distance.to.coast_km, distance_km, evi_hs, evi_5, evi_15, evi_25,
+                gls_tree_hs, gls_tree_5, gls_tree_15, gls_tree_25, gls_water_hs, gls_water_5, gls_water_15, gls_water_25)
+
+global_predictors <- round(cor(global_predictors), 2)
+
+global_predictors.2 <- analysis_all %>%
+  dplyr::select(Area_ha, distance.to.coast_km, distance_km, evi_hs, evi_5, evi_25,
+                gls_tree_hs, gls_tree_5, gls_tree_25, gls_water_hs, gls_water_5, gls_water_25)
+
+global_predictors.2 <- round(cor(global_predictors.2), 2)
+
+
+### removed the 15 km buffer due to collinearity issues
+
+### select only the necessary response and predictor variables to include in the analysis
+model_data <- analysis_all %>%
+  dplyr::select(Area_ha, distance.to.coast_km, distance_km, evi_hs, gls_tree_hs, gls_water_hs,
+                evi_5, gls_tree_5, gls_water_5, evi_25, gls_tree_25, gls_water_25, Urban_area, Country, 
+                hundred_km_buffer_SR, species_richness)
+
+### scale and center the predictor variables
+model_data[1:12] <- scale(model_data[1:12])
+
+### global model
+global.mod <- lmer(species_richness ~ Area_ha + distance.to.coast_km + distance_km + evi_hs + gls_tree_hs + gls_water_hs +
+                     evi_5 + gls_tree_5 + gls_water_5  + evi_25 + gls_tree_25 + gls_water_25 +
+                     (1|Urban_area) + (1|Country), REML=FALSE, data=model_data, na.action="na.fail")
+
+
+#### Plotting fitted vs residual values of model (homogeneity) ####
+plot(global.mod, add.smooth = FALSE, which = 1)
+
+#### Checking for normality of residuals of null model
+E <- resid(global.mod)              
+hist(E, xlab="Residuals", main="")
+
+summary(global.mod)
+display(global.mod)
+
+plot(Effect("Area_ha", global.mod))
+
+#### Automated Approach ####
+model.set <- dredge(global.mod) #### Computes all possible subsets of global model
+
+model.set
+
+top.models <- get.models(model.set, subset=delta<3) #### selects all models with deltaAic < 2
+
+length(top.models) ## how many top models there are
+
+#### only four top models in this case
+
+my.models <- model.sel(top.models) #### Ranks these models based on AICc
+
+print.data.frame(my.models, digits=2) #### Prints the top models (selected above)
+
+Averaged_models <- model.avg(top.models) #### Employs a model averaging technique for the top models using a full(zero method)
+
+summary(Averaged_models)
+confint(Averaged_models, full=TRUE)
+
+
+
+
+##########################################################
+##########################################################
+### 'standardised' species richness analysis #############
+##########################################################
+
+### global model
+global.mod_standard <- lmer(species_richness/hundred_km_buffer_SR ~ Area_ha + distance.to.coast_km + distance_km + evi_hs + 
+                            gls_tree_hs + gls_water_hs + evi_5 + gls_tree_5 + gls_water_5 + evi_25 + gls_tree_25 + gls_water_25 +
+                            (1|Urban_area) + (1|Country), REML=FALSE, data=model_data, na.action="na.fail")
+
+
+#### Plotting fitted vs residual values of model (homogeneity) ####
+plot(global.mod_standard, add.smooth = FALSE, which = 1)
+
+#### Checking for normality of residuals of null model
+E <- resid(global.mod_standard)              
+hist(E, xlab="Residuals", main="")
+
+summary(global.mod_standard)
+display(global.mod_standard)
+
+plot(Effect("Area_ha", global.mod_standard))
+
+#### Automated Approach ####
+model.set_standard <- dredge(global.mod_standard) #### Computes all possible subsets of global model
+
+model.set_standard
+
+top.models_standard <- get.models(model.set_standard, subset=delta<3) #### selects all models with deltaAic < 3
+#### only four top models in this case
+
+my.models_standard <- model.sel(top.models_standard) #### Ranks these models based on AICc
+
+print.data.frame(my.models_standard, digits=2) #### Prints the top four models (selected above)
+
+Averaged_models_standard <- model.avg(top.models_standard) #### Employs a model averaging technique for the top four models using a full(zero method)
+
+summary(Averaged_models_standard)
+confint(Averaged_models_standard, full=TRUE)
+
+
+
+###########################################################
+#### Making figures and/or tables for paper presentation ##
+###########################################################
+
+
+## figure of raw species richness versus greenspace size plotted
+base_breaks <- function(n = 10){
+  function(x) {
+    axisTicks(log10(range(x, na.rm = TRUE)), log = TRUE, n = n)
+  }
+}
+
+bks <- seq(min(analysis_all$Area_ha), max(analysis_all$Area_ha))
+
+ggplot(analysis_all, aes(x=Area_ha, y=species_richness))+
+  geom_point()+
+  theme_bw()+
+  xlab("Log(Area (Ha))")+
+  ylab("Species Richness")+
+  scale_x_log10(labels=comma)+
+  theme(axis.text.x=element_text(size=12, color="black"))+
+  theme(axis.text.y=element_text(size=12, color="black"))+
+  theme(axis.title.y=element_text(size=16))+
+  theme(axis.title.x=element_text(size=16))+
+  theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())+
+  theme(panel.grid.minor.y=element_blank(), panel.grid.major.y=element_blank())+
+  geom_smooth(method=lm)
+
+
+
+
+a <- as.data.frame(coefficients(Averaged_models, full=TRUE))
+b <- as.data.frame(confint(Averaged_models, full=TRUE))
+model_results <- cbind(a, b)
+names(model_results)[1] <- "estimate"
+names(model_results)[2] <- "lwr"
+names(model_results)[3] <- "upr"
+model_results$variable <- row.names(model_results)
+row.names(model_results) <- NULL
+
+
+ggplot(model_results, aes(x=variable, y=estimate, ymin=lwr, ymax=upr))+
+  geom_linerange(position=position_dodge(width=0.8))+
+  geom_point(color="blue")+
+  geom_hline(yintercept=0, color="red")+
+  coord_flip()+
+  theme_bw()+
+  xlab('Model Variable')+
+  ylab('Estimate')+
+  theme(axis.text.x=element_text(size=12))+
+  theme(axis.text.y=element_text(size=12))+
+  theme(axis.title.y=element_text(size=20))+
+  theme(axis.title.x=element_text(size=20))+
+  theme(panel.grid.minor.x=element_blank(), panel.grid.major.x=element_blank())+
+  theme(panel.grid.minor.y=element_blank(), panel.grid.major.y=element_blank())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/ANALYSIS_2.R
@@ -0,0 +1,195 @@
+#########################################################
+#########################################################
+################  ANALYSIS 2 ############################
+#########################################################
+#########################################################
+
+
+## setwd("H:/Dissertation/Dissertation Chapters/Data Chapters/Relationship between avian diversity and urban environments/Analysis/avian diversity in greenspaces")
+
+
+## First, load the RData file in
+load("Data/modelling_data.RData")
+
+### Read in bird data
+load("Data/Bird_data/bird_data.RData")
+
+### Packages
+library(dplyr)
+library(ggplot2)
+library(lme4)
+library(arm)
+library(MuMIn)
+library(effects)
+
+
+### Prepare data for modelling of "FINAL DATASET"
+
+## collapse all sites to polygon ids
+analysis_all <- predictor_variables
+analysis_all$species_richness <- as.numeric(analysis_all$species_richness)
+analysis_all <- na.exclude(analysis_all)
+
+
+
+################################
+################################
+## assess Species Richness first
+################################
+
+
+## join the file above with the bird data file
+tmp.model_data_SR <- Final_study_sites %>%
+  dplyr::select(Polygon_id, LOCALITY_ID) %>%
+  right_join(., analysis_all, by="Polygon_id") %>%
+  dplyr::select(LOCALITY_ID) %>%
+  inner_join(., species_richness, by="LOCALITY_ID") %>%
+  dplyr::select(SAMPLING_EVENT_IDENTIFIER, checklist_species_richness, OBSERVATION_DATE, PROTOCOL_TYPE, ALL_SPECIES_REPORTED,
+         DURATION_MINUTES, month, Polygon_id) %>%
+  inner_join(., analysis_all, by="Polygon_id")
+
+
+species_richness_summary <- tmp.model_data_SR %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER)))
+
+## remove any greenspaces which didn't have at least 50 checklists
+model_data_SR <- tmp.model_data_SR %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER))) %>%
+  inner_join(., tmp.model_data_SR, by="Polygon_id") %>%
+  filter(lists>=50)
+
+data_summary_SR <- model_data_SR %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER)))
+
+mean(data_summary_SR$lists)
+sd(data_summary_SR$lists)
+min(data_summary_SR$lists)
+max(data_summary_SR$lists)
+
+
+### select only the necessary response and predictor variables to include in the analysis
+model_data_SR <- model_data_SR %>%
+  dplyr::select(Area_ha, distance.to.coast_km, distance_km, evi_hs, gls_tree_hs, gls_water_hs,
+                evi_5, gls_tree_5, gls_water_5, evi_25, gls_tree_25, gls_water_25, DURATION_MINUTES, 
+                Urban_area, Country, hundred_km_buffer_SR, checklist_species_richness, SAMPLING_EVENT_IDENTIFIER, month,
+                Polygon_id)
+
+month <- data.frame(month=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+                    id=1:12)
+
+### change month to a character vector for modelling
+model_data_SR$month <- as.character(model_data_SR$month)
+
+
+### scale and center the predictor variables
+model_data_SR[1:13] <- scale(model_data_SR[1:13])
+
+hist(model_data_SR$checklist_species_richness)
+
+### global model
+global.mod <- lmer(checklist_species_richness ~ Area_ha + distance.to.coast_km + distance_km + evi_hs + gls_tree_hs + gls_water_hs +
+                     evi_5 + gls_tree_5 + gls_water_5  + evi_25 + gls_tree_25 + gls_water_25 + offset(DURATION_MINUTES) + (1|Polygon_id/month) +
+                     (1|Urban_area) + (1|Country), REML=FALSE, data=model_data_SR, na.action="na.fail")
+
+#### Plotting fitted vs residual values of model (homogeneity) ####
+plot(global.mod, add.smooth = FALSE, which = 1)
+
+#### Checking for normality of residuals of null model
+E <- resid(global.mod)              
+hist(E, xlab="Residuals", main="")
+
+summary(global.mod)
+display(global.mod)
+
+plot(Effect("Area_ha", global.mod))
+
+#### Automated Approach ####
+### Paralleslised ###
+library(snow)
+clusterType <- if(length(find.package("snow", quiet = TRUE))) "SOCK" else "PSOCK"
+clust <- try(makeCluster(getOption("cl.cores", 11), type = clusterType))
+
+clusterExport(clust, c("model_data_SR", "lmer"))
+
+
+model.set <- pdredge(global.mod, cluster=clust) #### Computes all possible subsets of global model
+
+model.set
+
+top.models <- get.models(model.set, subset=delta<3) #### selects all models with deltaAic < 3
+
+length(top.models) ## how many top models there are
+
+my.models <- model.sel(top.models) #### Ranks these models based on AICc
+
+print.data.frame(my.models, digits=2) #### Prints the top models (selected above)
+
+Averaged_models <- model.avg(top.models) #### Employs a model averaging technique for the top models using a full(zero method)
+
+summary(Averaged_models)
+confint(Averaged_models, full=TRUE)
+
+
+################################
+################################
+## Assess Species Diversity 2nd
+################################
+
+## join the file above with the bird data file
+tmp.model_data_SD <- Final_study_sites %>%
+  dplyr::select(Polygon_id, LOCALITY_ID) %>%
+  right_join(., analysis_all, by="Polygon_id") %>%
+  dplyr::select(LOCALITY_ID) %>%
+  inner_join(., species_diversity, by="LOCALITY_ID") %>%
+  dplyr::select(SAMPLING_EVENT_IDENTIFIER, species_diversity, OBSERVATION_DATE, PROTOCOL_TYPE, ALL_SPECIES_REPORTED,
+                DURATION_MINUTES, month, Polygon_id) %>%
+  inner_join(., analysis_all, by="Polygon_id")
+
+
+species_diversity_summary <- tmp.model_data_SD %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER)))
+
+## remove any greenspaces which didn't have at least 50 checklists
+model_data_SD <- tmp.model_data_SD %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER))) %>%
+  inner_join(., tmp.model_data_SD, by="Polygon_id") %>%
+  filter(lists>=50)
+
+data_summary_SD <- model_data_SD %>%
+  group_by(Polygon_id) %>%
+  summarise(lists=length(unique(SAMPLING_EVENT_IDENTIFIER)))
+
+mean(data_summary_SD$lists)
+sd(data_summary_SD$lists)
+min(data_summary_SD$lists)
+max(data_summary_SD$lists)
+
+
+### select only the necessary response and predictor variables to include in the analysis
+model_data_SD <- model_data_SD %>%
+  dplyr::select(Area_ha, distance.to.coast_km, distance_km, evi_hs, gls_tree_hs, gls_water_hs,
+                evi_5, gls_tree_5, gls_water_5, evi_25, gls_tree_25, gls_water_25, DURATION_MINUTES, 
+                Urban_area, Country, hundred_km_buffer_SR, species_diversity, SAMPLING_EVENT_IDENTIFIER, month,
+                Polygon_id)
+
+month <- data.frame(month=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+                    id=1:12)
+
+### change month to a character vector for modelling
+model_data_SD$month <- as.character(model_data_SD$month)
+
+
+### scale and center the predictor variables
+model_data_SD[1:13] <- scale(model_data_SD[1:13])
+
+hist(model_data_SD$species_diversity)
+
+
+
+
+
